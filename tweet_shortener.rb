5c53cc9a8e93bf04de214d08181795940dd084e0
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-21 18:27:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,9 +14,12 @@ def dictionary
    " and " => " & "
   }
 end
+def word_substituter(tweet_string)
+  
+end
 
 =begin
-  
+def word_substituter(tweet_string)
   dictionary.each do |key, value|
     while tweet_string.include? key
         tweet_string[key] = value
