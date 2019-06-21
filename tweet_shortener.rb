aac41bca7228c1126f0aaed60b1458d3d37950c1
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-21 18:38:06 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,7 @@ def word_substituter(tweet_string)
   array_tweet.each_with_index do |word, index|
     dictionary.each do |key, value|
       if word == key
-        word.replace(value)
+        tweet[index]
       end
     end
   end
