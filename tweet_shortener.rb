3d4e1fea449ecf5e854e8e2808092c85010e79bb
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-21 18:20:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+# Write your code here. 
+require 'pry'
+def dictionary
+  dictionary = 
+  {" hello " => ' hi ', 
+   " too " => ' 2 ',
+   " to " => ' 2 ',
+   " two " => ' 2 ',
+   " for " => ' 4 ',
+   " four " => ' 4 ',
+   " be " => ' b ',
+   " you " => ' u ',
+   " at " => " @ ",
+   " and " => " & "
+  }
+end
+
+def word_substituter(tweet_string)
+  dictionary.each do |key, value|
+    while tweet_string.include? key
+        tweet_string[key] = value
+    end
+  end
+  tweet_string
+end
+
+def bulk_tweet_shortener(array_tweets)
+  array_tweets.each {|tweet| puts word_substituter(tweet)}
+end
